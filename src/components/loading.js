02f84964910aad3a22c565e9706a521ed27979fc
samiraomidvar7129 <93695009+samiraomidvar7129@users.noreply.github.com>
2024-01-13 02:34:03 -0800

    removed react spinner

--- src/components/loading.js
+++ src/components/loading.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import '../index.css'
-import { useState, CSSProperties } from "react";
-import {HashLoader} from "react-spinners";
 
 
-
-const overrid= CSSProperties = {
-    display: "block",
-    margin: "0 auto",
-    
-  };
-  
 function Loading() {
-    let [loading, setLoading] = useState(true);
-    let [color, setColor] = useState("#36d7b7");
+
   return (
     <div className="sweet-loading" id='loading-parent'>
-      
-      <HashLoader
-        size={40}
-        color={color}
-        aria-label="Loading "
-        data-testid="loader"
-      />
+      <h4>درحال بارگذاری ....</h4>
     </div>
   );
 }
