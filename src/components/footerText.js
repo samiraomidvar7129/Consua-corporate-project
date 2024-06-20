24ecf7f3c5ac949a09c452ca18f85acb49ceb573
samiraomidvar7129 <93695009+samiraomidvar7129@users.noreply.github.com>
2024-06-20 07:05:31 -0700

    change footer-item

--- src/components/footerText.js
+++ src/components/footerText.js
@@ -6,10 +6,10 @@ import SocialNetworks from "./socialNetworks";
 const FooterText = () => {
   return (
     <div className="footer-item-txt">
-       <div className="footer-item-img">
-       <h2>آریا گستر</h2>
+       
+       <h2> خبرنامه</h2>
 
-          </div>
+        
           <p className="footer-item-p">
           در شرکت راه حل فناوری اطلاعات ما متعهد به ارائه خدمات و پشتیبانی  استثنائی به مشتریان خود هستیم 
           </p>
