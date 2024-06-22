49f92cb5e0d04d4dc2922eb931c227ce577b062c
samiraomidvar7129 <93695009+samiraomidvar7129@users.noreply.github.com>
2024-06-22 05:11:32 -0700

    create news.js file

---
+++ src/components/news.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const News = () => {
+  return (
+    <div className='news-parent'>
+        <div>
+            
+        </div>
+
+      
+    </div>
+  )
+}
+
+export default News;
