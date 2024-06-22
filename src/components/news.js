beb41e1353bba319e41337b5c9a2fa2f9cd56b6f
samiraomidvar7129 <93695009+samiraomidvar7129@users.noreply.github.com>
2024-06-22 05:13:20 -0700

    create news-content section

--- src/components/news.js
+++ src/components/news.js
@@ -3,9 +3,10 @@ import React from 'react'
 const News = () => {
   return (
     <div className='news-parent'>
-        <div>
-            
+        <div className='news-title'>
+            <h3>اخبار و بروز رسانی ها</h3>
         </div>
+        <div className='news-content'></div>
 
       
     </div>
