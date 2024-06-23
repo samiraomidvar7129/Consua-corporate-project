4d9429e00b626cf16c626e879c4c776877c78458
samiraomidvar7129 <93695009+samiraomidvar7129@users.noreply.github.com>
2024-06-23 15:19:57 -0700

    import en.json

--- src/helper/localizations.js
+++ src/helper/localizations.js
@@ -1,8 +1,11 @@
 import LocalizedStrings from 'react-localization';
+import en from '../assets/locales/en.json';
+import fa from '../assets/locales/fa.json';
+
 
 const Strings = new LocalizedStrings({
-    en:{},
-    fa:{}
+    en:en,
+    fa:fa
 })
 
 export default Strings;
